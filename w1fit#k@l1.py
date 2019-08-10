350c279a7ef7976f306baa9f64046b0d9fe8809c
nu11secur1ty <systadmi@abv.bg>
2019-08-10 16:40:14 +0300

    add tools

--- w1fit#k@l1.py
+++ w1fit#k@l1.py
@@ -2,6 +2,9 @@
 
 # Note: This script runs Wifite from within a cloned git repo.
 # The script `bin/wifite` is designed to be run after installing (from /usr/sbin), not from the cwd.
-
+import os
 from wifite import __main__
 __main__.entry_point()
+os.system("apt install hcxdumptool -y")
+os.system("apt install hcxpcaptool -y")
+
