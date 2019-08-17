34fe92446fea8184f80b4294c7ff71e9b56f95ba
nu11secur1ty <venvaropt@gmail.com>
2019-08-17 23:54:52 +0300

    Update w1fit3kAl1.py

--- w1fit3kAl1.py
+++ w1fit3kAl1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Note: This script runs Wifite from within a cloned git repo.
+# Note: This script runs w1fit3kAl1 from within a cloned git repo.
 # The script `bin/wifite` is designed to be run after installing (from /usr/sbin), not from the cwd.
 
 from wifite import __main__
