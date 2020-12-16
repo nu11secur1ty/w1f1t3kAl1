587ac200d776b458ab2f6f79176acebe5c2b54b8
nu11secur1ty <venvaropt@gmail.com>
2020-12-16 19:27:53 +0200

    fix_local_integrated_wifi_interface

---
+++ local_wifi_fix_monitoring/shurikeni.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+
+print "This is your wifi interfaces\n";
+my $your_interfaces = `iwconfig`;
+	print "$your_interfaces\n";
+print "Give your wifi integrated interface?\n";
+	my $target = <STDIN>;
+	my $mon_stop_wifi = `airmon-ng stop $target`;
+	my $mon_interface = `airmon-ng start $target`;
+
+	exit 0;
