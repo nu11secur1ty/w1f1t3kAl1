7f29447c5559c340c206ac6d9eb9bc5ef464c229
nu11secur1ty <venvaropt@gmail.com>
2020-12-27 18:33:52 -0500

    Update installer.sh

--- install_tools/Pyrit/installer.sh
+++ install_tools/Pyrit/installer.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/bash
-git clone https://github.com/JPaulMora/Pyrit.git
-  cd Pyrti
-  apt-get install libpcap-dev
-  ./setup.py build install
+# @nu11secur1ty
+# Kali Linux 2020.x
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install python2.7-dev libssl-dev zlib1g-dev libpcap-dev -y
+sudo apt-get install libpcap-dev -y
+  apt-get remove --purge pyrit
+  rm -r /usr/local/lib/python2.7/dist-packages/cpyrit/
+sudo pip install psycopg2
+sudo pip install scapy
+sudo apt-get install python-scapy -y
+echo "Downloading Pyrit"
+printf '\033]2;Downloading Pyrit\a'
+  git clone https://github.com/JPaulMora/Pyrit.git
+  cd Pyrit
+    python setup.py clean
+    python setup.py build
+    sudo python setup.py install
+echo "Installation finished"
+printf '\033]2; Installation finished\a'
     exit 0
