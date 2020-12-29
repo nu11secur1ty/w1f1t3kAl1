4c1194038ec7e2a35cbeae62c4795e03a6a1f2fc
root <root@kali.nu11secur1ty.com>
2020-12-28 22:33:43 -0500

    monitoring

--- airmon/airmon.pl
+++ airmon/airmon.pl
@@ -2,12 +2,21 @@
 use strict;
 use warnings;
 use diagnostics;
+use Term::ANSIColor;
 
+print color('bold blue');
 print "This is your wifi interfaces\n";
+	print color('reset');
+
 my $your_interfaces = `iwconfig`;
+	print color('bold yellow');
 	print "$your_interfaces\n";
+	print color('reset');
+
+print color('bold red');
 print "Give your wifi integrated interface?\n";
 	my $target = <STDIN>;
 	my $mon_stop_wifi = `airmon-ng stop $target`;
 	my $mon_interface = `airmon-ng start $target`;
+		print color('reset');
 	exit 0;
